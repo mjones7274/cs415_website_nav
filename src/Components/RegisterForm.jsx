eba4c4c393d2f297a4bfcb0a8f0ef05137e6d85a
mjones7274 <mdj7274@gmail.com>
2024-03-01 13:40:12 -0700

    Fixed Registration issue

--- src/Components/RegisterForm.jsx
+++ src/Components/RegisterForm.jsx
@@ -18,7 +18,7 @@ export const RegisterForm = (props) => {
             first_name: firstName,
             last_name: lastName,
             email: email,
-            pass_word: pass
+            password: pass
         })
         try {
             fetch(process.env.REACT_APP_API_URL_BASE + '/users/',{
